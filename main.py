93e324ea6b2912c22ee968c9f8f810af40e7182c
Tyler Davis <coding@tydavis.dev>
2024-01-02 22:36:55 -0800

    add print results, save results to output.txt fix #8 and fix #7

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import math
+import os.path
+from win32 import win32print
+import win32api
 
 version = "v0.2.0"
+
+outputFileName = "./trip-packing-calculator/output.txt"
 
 def daysFunction():
     while True:
@@ -45,8 +50,26 @@ def washerFunction():
     else:
         print("Make sure to input a Y or an N")
         washerFunction()
+def printFunction():
+    printResults = input('\nDo you want to print these results? (y/N):\n')
+    if printResults == "y" or printResults == "Y":
+        printer_name = win32print.GetDefaultPrinter()
+        hPrinter = win32print.OpenPrinter(printer_name)
+        filename = "output.txt"
+        try:
+            hJob = win32print.StartDocPrinter(hPrinter, 1, ('PrintJobName', None, 'TEXT'))
+            try:
+                win32api.ShellExecute(0, "print", filename, None, "./trip-packing-calculator", 0)
+            finally:
+                win32print.EndDocPrinter(hPrinter)
+        finally:
+            win32print.ClosePrinter(hPrinter)
+    elif printResults == "n" or printResults == "N" or printResults == "":
+        print()
+    else:
+        print()
 
-print("Trip Packing Calculator\n")
+print("Trip Packing Calculator")
 print("You are using", version)
 print()
 print()
@@ -59,48 +82,60 @@ formalVar = formalFunction()
 gymVar = gymFunction()
 washerVar = washerFunction()
 
-print("You should pack:")
+file_exists = os.path.exists('./trip-packing-calculator/output.txt')
+if not file_exists:
+    path = './trip-packing-calculator'
+    os.mkdir(path)
 
-if washerVar == True:
-    if days >= 7:
-        print("7 normal outfits")
-        print("1 set of PJ's")
-    elif days < 7:
-        print(days, "normal outfit(s)")
-        print("1 set of PJ's")
-    if waterVar == True:
-        if nights >= 7:
-            print("7 sets of swim wear")
-        elif nights < 7:
-            print(nights, "sets of swim wear")
-    if formalVar == True:
-        print("1 suit")
-        print("1 set of formal shoes")
-        print("1 tie")
-        print("1 formal belt")
-    if gymVar == True:
-        if nights >= 7:
-            print("7 sets of gym wear")
-        elif nights < 7:
-            print(nights, "set(s) of gym wear")
-elif washerVar == False:
-    print(days, "normal outfit(s)")
-    print(math.ceil(nights/7), "set(s) of PJ's")
-    if waterVar == True:
-        print(nights, "set's of swim wear")
-    if formalVar == True:
-        print("1 suit")
-        print("1 set of formal shoes")
-        print("1 tie")
-        print("1 formal belt")
-    if gymVar == True:
-        print(nights, "set(s) of gym wear")
-print("Toothbrush(s)")
-print("Toothpaste(s)")
-print("Dental floss(s)")
-print("Shaving cream(s)")
-print("Razor(s)")
-print("Deodorant(s)")
-print("Cologne")
-print("Body soap(s)")
-print("Shampoo(s)")
+with open(outputFileName, "w+") as f:
+    print()
+    print("You should pack:", file=f)
+    if washerVar == True:
+        if days >= 7:
+            print("7 normal outfits", file=f)
+            print("1 set of PJ's", file=f)
+        elif days < 7:
+            print(days, "normal outfit(s)", file=f)
+            print("1 set of PJ's", file=f)
+        if waterVar == True:
+            if nights >= 7:
+                print("7 sets of swim wear", file=f)
+            elif nights < 7:
+                print(nights, "sets of swim wear", file=f)
+        if formalVar == True:
+            print("1 suit", file=f)
+            print("1 set of formal shoes", file=f)
+            print("1 tie", file=f)
+            print("1 formal belt", file=f)
+        if gymVar == True:
+            if nights >= 7:
+                print("7 sets of gym wear", file=f)
+            elif nights < 7:
+                print(nights, "set(s) of gym wear", file=f)
+    elif washerVar == False:
+        print(days, "normal outfit(s)", file=f)
+        print(math.ceil(nights/7), "set(s) of PJ's", file=f)
+        if waterVar == True:
+            print(nights, "set(s) of swim wear", file=f)
+        if formalVar == True:
+            print("1 suit", file=f)
+            print("1 set of formal shoes", file=f)
+            print("1 tie", file=f)
+            print("1 formal belt", file=f)
+        if gymVar == True:
+            print(nights, "set(s) of gym wear", file=f)
+    print("Toothbrush(s)", file=f)
+    print("Toothpaste(s)", file=f)
+    print("Dental floss(s)", file=f)
+    print("Shaving cream(s)", file=f)
+    print("Razor(s)", file=f)
+    print("Deodorant(s)", file=f)
+    print("Cologne", file=f)
+    print("Body soap(s)", file=f)
+    print("Shampoo(s)", file=f)
+    f.close
+with open(outputFileName, "r") as f:
+    print(f.read())
+    f.close
+
+printFunction()
